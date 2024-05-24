81256e51c4c60921e0c2ebc4021a5e5cbf86a186
Susantha Perera <33259705+mskperera@users.noreply.github.com>
2024-05-24 15:53:14 +0530

    modified allowed orgin in.env

--- server.js
+++ server.js
@@ -20,8 +20,29 @@ app.use(cookieParser());
 
 //cors
 //if (process.env.NODE_ENV === "development") {
-app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+//app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 //}
+// Extract allowed origins from environment variable
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Allow requests with no origin, like mobile apps or curl requests
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Enable cookies and other credentials
+};
+
+// Use CORS middleware
+app.use(cors(corsOptions));
+
 
 //reoutes middlewares
 
