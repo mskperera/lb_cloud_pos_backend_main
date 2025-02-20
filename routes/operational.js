6fbd8218b4968400a8ba9d8485bb376567d7aa6f
Susantha Perera <33259705+mskperera@users.noreply.github.com>
2025-02-21 00:39:16 +0530

    Protecting Inner API Endpoints

--- routes/operational.js
+++ routes/operational.js
@@ -3,6 +3,7 @@ const { initializeDbAndConnection_ctrl, cleanupIncompleteTenancySetups_ctrl,
      removeTenancySetup_ctrl, restoreDatabase_ctrl, backupDatabaseTo_live_db_dumps_ctrl,
       removeDBUserAccount_ctrl, createUserAccountForDb_ctrl, 
       getConnectionDetailsByTenantId_ctrl} = require("../controllers/operational");
+const { apiProtectMiddleware } = require("../middlewares/apiProtect");
  const router = express.Router();
 
  router.post("/operational/restoreDatabase", restoreDatabase_ctrl);
@@ -13,6 +14,6 @@ const { initializeDbAndConnection_ctrl, cleanupIncompleteTenancySetups_ctrl,
  router.post("/operational/removeTenancySetup", removeTenancySetup_ctrl);
  router.post("/operational/create-user-account-for-db", createUserAccountForDb_ctrl);
  
- router.get("/operational/getConnectionDetailsByTenantId", getConnectionDetailsByTenantId_ctrl);
+ router.get("/operational/getConnectionDetailsByTenantId",apiProtectMiddleware, getConnectionDetailsByTenantId_ctrl);
 
 module.exports = router;
